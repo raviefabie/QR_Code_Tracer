fda7c4f57760b3318b449f3f625355b155c0eac7
raviefabie <48237519+raviefabie@users.noreply.github.com>
2021-01-20 15:19:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/qr_code_tracer/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.qr_code_tracer;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
